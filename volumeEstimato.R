0f75dedd42b2d364f01a80fc18e468b80a0fa36d
anoukdimbeanu <20ad57@queensu.ca>
2025-01-07 15:05:15 -0500

    Added volume estimation to limb measurements csv

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -5,4 +5,4 @@ dat <- read.csv("measurements.csv")
 # insert column for volume using mutate function and volume of a cylinder formula
 dat<- dat %>% mutate(volume = pi*(Limb_width/2)^2*Limb_length)
 
-
+write.csv(dat,"measurements.csv",row.names=FALSE)
